aa90f6e8248a29287f42e74d525ce4f99425de01
Manas Prakash <manasp2004@gmail.com>
2020-09-10 17:57:22 +0530

    initial commit

---
+++ src/Main.java
@@ -0,0 +1,34 @@
+import java.util.Scanner;
+
+public class Main {
+
+    private static Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        int testArr[] = getNumbers(5);
+        minimumElement(testArr);
+    }
+
+    public static int[] getNumbers(int number) {
+        int arr[] = new int[number];
+        for (int i = 0; i < arr.length; i++) {
+            int count = i + 1;
+            System.out.println("Enter " + count +  " number");
+            arr[i] = scanner.nextInt();
+//            System.out.println(arr[i]);
+        }
+        return arr;
+    }
+
+    public static void minimumElement(int arr[]) {
+        int minimumElement = Integer.MAX_VALUE;
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] < minimumElement) {
+                minimumElement = arr[i];
+            }
+        }
+        System.out.println("Minimum element is " + minimumElement);
+
+
+    }
+}
